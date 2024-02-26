1ec1d04659f6d4275934a7f2878d86baeb1d0761
Olivier Certner <olce@FreeBSD.org>
2024-02-26 19:12:42 +0100

    runq: Deduce most parameters, remove machine headers
    The 'runq' machinery now depends on only two settable parameters,
    RQ_MAX_PRIO, the maximum priority number that can be accepted, the
    minimum being 0, and RQ_PPQ, the number of priorities per queue (to
    reduce the number of queues).
    
    All other parameters are deduced from these ones.  Also, all
    architectures automatically get a runq word that is their natural word.
    
    RQB_FFS() always was 'ffsl() - 1' except for amd64 where it was
    'bsfq()'.  Now that all these finally call compiler builtins, the
    resulting assembly code is the same, so there is no cost to removing
    this special case.
    
    After all these changes, <machine/runq.h> headers have no more purpose,
    so remove them.
    
    While here, fix potentially confusing parameter name for RQB_WORD() and
    RQB_BIT().
    
    No functional change (intended).

--- sys/sys/runq.h
+++ sys/sys/runq.h
@@ -29,16 +29,36 @@
 #ifndef	_RUNQ_H_
 #define	_RUNQ_H_
 
-#include <machine/runq.h>
-
 struct thread;
 
 /*
  * Run queue parameters.
  */
 
-#define	RQ_NQS		(64)		/* Number of run queues. */
-#define	RQ_PPQ		(4)		/* Priorities per queue. */
+#define	RQ_MAX_PRIO	(255)	/* Maximum priority (minimum is 0). */
+#define	RQ_PPQ		(4)	/* Priorities per queue. */
+
+/*
+ * Convenience macros from <sys/param.h>.
+ */
+#ifndef	NBBY
+#define	NBBY 8
+#endif
+#ifndef	howmany
+#define	howmany(x, y)	(((x)+((y)-1))/(y))
+#endif
+
+/*
+ * Deduced from the above parameters and machine ones.
+ */
+typedef	unsigned long	rqb_word_t;	/* runq's status words type. */
+
+#define	RQ_NQS	(howmany(RQ_MAX_PRIO + 1, RQ_PPQ)) /* Number of run queues. */
+#define	RQB_BPW	(sizeof(rqb_word_t) * NBBY) /* Bits per runq word. */
+#define	RQB_LEN	(howmany(RQ_NQS, RQB_BPW)) /* Words to cover RQ_NQS queues. */
+#define	RQB_WORD(idx)	((idx) / RQB_BPW)
+#define	RQB_BIT(idx)	(1ul << ((idx) % RQB_BPW))
+#define	RQB_FFS(word)	(ffsl((long)(word)) - 1) /* Assumes two-complement. */
 
 /*
  * Head of run queues.
