832f03433ca3e31b26aa04f089dbc74eb848e330
Olivier Certner <olce@FreeBSD.org>
2024-03-28 14:32:17 +0100

    runq: Hide function prototypes under _KERNEL
    And some structure definitions as well.
    
    This header really is not supposed to be included by userland, so should
    just error in this case.  However, there is one remaining use for it in
    a test: Getting the value of RQ_PPQ to ensure a big enough priority
    level difference in order to guarantee that a realtime thread preempts
    another.  This use will soon be obsoleted by guaranteeing that
    a realtime thread always preempts another one with lower priority, even
    if the priority level is very close.

--- sys/sys/runq.h
+++ sys/sys/runq.h
@@ -29,8 +29,6 @@
 #ifndef	_RUNQ_H_
 #define	_RUNQ_H_
 
-struct thread;
-
 /*
  * Run queue parameters.
  */
@@ -59,6 +57,10 @@ typedef	unsigned long	rqb_word_t;	/* runq's status words type. */
 #define	RQB_WORD(idx)	((idx) / RQB_BPW)
 #define	RQB_BIT(idx)	(1ul << ((idx) % RQB_BPW))
 #define	RQB_FFS(word)	(ffsl((long)(word)) - 1) /* Assumes two-complement. */
+
+
+#ifdef _KERNEL
+struct thread;
 
 /*
  * Head of run queues.
@@ -91,5 +93,6 @@ struct	thread *runq_choose_fuzz(struct runq *, int);
 void	runq_init(struct runq *);
 void	runq_remove(struct runq *, struct thread *);
 void	runq_remove_idx(struct runq *, struct thread *, u_char *);
+#endif /* _KERNEL */
 
 #endif
